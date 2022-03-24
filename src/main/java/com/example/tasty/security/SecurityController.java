58940af37f5926145621d02db657354c44ddb4b3
Chmielu <j.j.chmielewski@wp.pl>
2022-03-24 17:28:42 +0100

    Cors 2

--- src/main/java/com/example/tasty/security/SecurityController.java
+++ src/main/java/com/example/tasty/security/SecurityController.java
@@ -7,7 +7,7 @@ import org.springframework.web.bind.annotation.*;
 
 @RestController
 @RequestMapping("/api")
-@CrossOrigin(origins = {"http://127.0.0.1:5500", "http://192.168.0.136:5500"})
+@CrossOrigin(origins = {"http://127.0.0.1:5500", "http://192.168.0.136:5500", "https://jjchmielewski.github.io/TastyFrontendApp", "https://jjchmielewski.github.io"})
 public class SecurityController {
 
     private final WebTokenHandler webTokenHandler;
